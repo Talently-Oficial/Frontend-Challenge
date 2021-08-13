792b5c50579838f1a2a17d327f943dd92b10494a
Fernando Mellone <fernandomellone2@gmail.com>
2021-08-13 13:13:23 -0400

    Added Status cheap component

---
+++ components/LyStatusCheap.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="rounded-md px-2 text-center" :class="colors[text] || colors.default">
+    <span class="text-white">{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // TODO document this "colors" property
+    colors: {
+      type: Object,
+      default: () => ({
+        Alive: 'bg-green-400',
+        Death: 'bg-red-400',
+        unknown: 'bg-gray-400'
+      })
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
