722846e7c34c5407932ce432cbd397edc392e650
Fernando Mellone <fernandomellone2@gmail.com>
2021-08-13 13:10:55 -0400

    Added avatar component

---
+++ components/LyAvatar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex align-items-center">
+    <img class="rounded-full h-12 w-12  object-cover" :src="image" alt="unsplash image">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: ''
+    },
+    alternativeImage: {
+      type: String,
+      default: '/images/alternative-image.png'
+    }
+  },
+  computed: {
+    image () {
+      return this.src || this.alternativeImage
+    }
+  }
+}
+</script>
