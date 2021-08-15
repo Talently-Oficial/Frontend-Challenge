7fe112d65c50a7e6477b156b94bade248c48075f
femellone <goresetpy@protonmail.com>
2021-08-15 17:49:56 -0400

    Added Character form component

---
+++ components/CharacterForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="CharacterForm">
+    <div class="mt-8">
+      <div class="grid grid-cols-1 gap-6">
+        <LyInputText v-model="name" label="Name" />
+        <LyInputSelect v-model="status" label="Status" :options="statuses" />
+        <label class="block">
+          <span class="text-gray-700">Created</span>
+          <input type="date" class="mt-1 w-full block rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      status: null,
+      statuses: [
+        {
+          text: 'Alive',
+          value: 'Alive'
+        },
+        {
+          text: 'Dead',
+          value: 'Dead'
+        },
+        {
+          text: 'unknown',
+          value: 'unknown'
+        }
+      ]
+    }
+  },
+  mounted () {
+    this.fetchInitialFields()
+  },
+  methods: {
+    fetchInitialFields () {
+      this.status = this.statuses[2]
+    }
+  }
+}
+</script>
