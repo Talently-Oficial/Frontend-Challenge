29328e27503e1d724b36eb18658d1d54e1357eaa
Fernando Mellone <fernandomellone2@gmail.com>
2021-08-15 01:46:28 -0400

    Added button component

---
+++ components/LyButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button type="button" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" @click="onClick">
+    {{ innerText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    innerText: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    onClick () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
