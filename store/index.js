9666e6403d41fcc392d9da4fc91e9418a74c0966
Fernando Mellone <fernandomellone2@gmail.com>
2021-08-16 01:37:28 -0400

    Added logic to alert component

--- store/index.js
+++ store/index.js
@@ -1,9 +1,43 @@
+const initialAlertState = {
+  type: '',
+  message: ''
+}
+
 export const state = () => ({
-  fetchPending: false
+  fetchPending: false,
+  alertOptions: initialAlertState
 })
 
 export const mutations = {
   setFetchPendingState (state, newState) {
     state.fetchPending = newState
+  },
+
+  showAlert (state, options) {
+    state.alertOptions = { ...options }
+  },
+
+  resetAlert (state) {
+    state.alertOptions = initialAlertState
+  }
+}
+
+export const actions = {
+  showSuccessAlert ({ commit }, message) {
+    const options = {
+      type: 'success',
+      message
+    }
+
+    commit('showAlert', options)
+  },
+
+  showErrorAlert ({ commit }, message) {
+    const options = {
+      type: 'error',
+      message
+    }
+
+    commit('showAlert', options)
   }
 }
