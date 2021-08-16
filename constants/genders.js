e4d59e7975375c82907dc26baeebc487f6a90571
Fernando Mellone <fernandomellone2@gmail.com>
2021-08-15 20:58:45 -0400

    Added constants for genders and statuses selectors

---
+++ constants/genders.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    text: 'Female',
+    value: 'Female'
+  },
+  {
+    text: 'Male',
+    value: 'Male'
+  },
+  {
+    text: 'Genderless',
+    value: 'Genderless'
+  },
+  {
+    text: 'unknown',
+    value: 'unknown'
+  }
+]
---
+++ constants/statuses.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    text: 'Alive',
+    value: 'Alive'
+  },
+  {
+    text: 'Dead',
+    value: 'Dead'
+  },
+  {
+    text: 'unknown',
+    value: 'unknown'
+  }
+]
