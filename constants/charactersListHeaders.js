7a0bf03ca633e2fc841f33de6a1e2a208b488d39
Fernando Mellone <fernandomellone2@gmail.com>
2021-08-16 01:38:47 -0400

    Added characters list header constant array

---
+++ constants/charactersListHeaders.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    title: 'Image',
+    customClass: ''
+  },
+  {
+    title: 'Name',
+    customClass: 'text-left'
+  },
+  {
+    title: 'Status',
+    customClass: 'hidden sm:table-cell'
+  },
+  {
+    title: 'Species',
+    customClass: ''
+  },
+  {
+    title: 'Gender',
+    customClass: 'hidden sm:table-cell'
+  },
+  {
+    title: 'Created',
+    customClass: 'hidden sm:table-cell'
+  },
+  {
+    title: '',
+    customClass: ''
+  }
+]
