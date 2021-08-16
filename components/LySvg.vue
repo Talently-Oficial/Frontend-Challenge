5ab051f76dd183d22697621bbdb7fe5a2507e551
Fernando Mellone <fernandomellone2@gmail.com>
2021-08-16 01:38:27 -0400

    Removed the default color of SVG component

--- components/LySvg.vue
+++ components/LySvg.vue
@@ -2,7 +2,7 @@
   <!-- eslint-disable vue/no-v-html -->
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-6 w-6 text-blue-500"
+    class="h-6 w-6"
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor"
